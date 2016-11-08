9be7f9e89442d25e528364c1ab93fbea6ee08ec6
thecichos <thecichos@gmail.com>
2016-11-08 13:06:32 +0100

    yeah this is more

--- js/_main.js
+++ js/_main.js
@@ -1,6 +1,22 @@
 var scene = new THREE.Scene();
-var cam = new THREE.PerspectiveCamera(75,window.innerWidth() / window.innerHeight(), 0.1,1000)
+var cam = new THREE.PerspectiveCamera(75,window.innerWidth() / window.innerHeight(), 0.1,1000);
 
-var renderer = new THREE.WebGlRenderer();
+var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth(), window.innerHeight());
 document.body.appendChild(renderer.domElement);
+
+var geometry = new THREE.BoxGeometry(1,1,1);
+var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube)
+
+cam.position.z = 5;
+
+
+var render = function () {
+	requestAnimationFrame(render)
+
+	renderer.render(scene, cam)
+}
+
+render()
